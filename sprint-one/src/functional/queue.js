66c08fe5df8e7385e440e28dfad4e2803c0998a7
BrianLoughnane <BrianJLoughnane@gmail.com>
2015-05-13 16:50:41 -0700

    Functional-shared stack complete

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,24 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var length = 0;
+  var counter = 0; 
 
   // Implement the methods below
-
   someInstance.enqueue = function(value){
+    storage[counter] = value;
+    counter++;
   };
 
   someInstance.dequeue = function(){
+    var firstKey = +Object.keys(storage)[0];
+    var firstValue = storage[firstKey];
+    delete storage[firstKey];
+    return firstValue;
   };
 
   someInstance.size = function(){
+    return Object.keys(storage).length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,27 @@ var Stack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var length = 0; 
+
+  var setLength = function() {
+    length = Object.keys(storage).length;
+  }
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[length] = value;
+    setLength();
   };
 
   someInstance.pop = function(){
+    var last = storage[length-1];
+    delete storage[length-1];
+    setLength()
+    return last;
   };
 
   someInstance.size = function(){
+    return length;
   };
 
   return someInstance;
