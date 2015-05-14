5e5d1c80e9396df57e173976457ef8e8d7040dee
tchan247 <tchan247@gmail.com>
2015-05-14 11:20:50 -0700

    Completed pseudoclassical stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,115 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.top = 0;
+  this.storage = {};
+
 };
 
+ Stack.prototype.push = function (value) {
+   this.storage[this.top] = value;
+   this.top++;
+ };
+
+ Stack.prototype.pop = function () {
+   if(this.top > 0) {
+     this.top--;
+     var result = this.storage[this.top];
+     delete this.storage[this.top];
+     return result;
+   }
+ };
+
+ Stack.prototype.size = function () {
+   return this.top;
+ };
+
+
+
+
+
+
+// var inst = new Stack();
+// inst.__proto__  = Stack.prototype;
+
+// inst.push();
+// inst.__proto__.push()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // var methods = {
+ //   push : function (value) {
+ //     this.storage[top] = value;
+ //     top++;
+ //   },
+ //   pop : function () {
+ //     if(top > 0) {
+ //       top--;
+ //       var result = this.storage[top];
+ //       delete this.storage[top];
+ //       return result;
+ //     }
+ //   },
+ //   size : function () {
+ //     return top;
+ //   }
+ // }
+
+
+
+// log = function () { console.log( this ) ;}
+// "name".log();
+// log.apply("brian");
+
+// newInstance.Stack()
+
+// new (newInstance).Stack()
+
+// var inst = new Stack();
+// inst.size
+// inst.pop()
+
+
+// does extend the constructor function's prototype ‣
+// AssertionError: expected 0 to be above 0
+
+// var prototypeObjectPropertyCount = Object.keys(constructor.prototype).length;
+
+// assuming(extendsPrototype).expect(prototypeObjectPropertyCount).to.be.above(0);
+
+// assuming(referencesThis).expect(/^((?!\/\/).)*(this)/.test(constructor)).to.be.true;
 
