c15a4867f3254106f7ebff6a5a1063c02faa06d1
tchan247 <tchan247@gmail.com>
2015-05-15 12:18:45 -0700

    Completed linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,16 +2,38 @@ var LinkedList = function(){
   var list = {};
   list.head = null;
   list.tail = null;
+  var first = 0;
+  var last = 0;
 
   list.addToTail = function(value){
+    var obj = new Node(value);
+    if(last > 0) {
+      list[last-1].next = obj;
+    }
+    list[last] = obj;
+    list.tail = obj;
+    last++;
+    list.head = list[first];
   };
 
   list.removeHead = function(){
+    var obj = list[first];
+    delete list[first];
+    first++;
+    list.head = list[first];
+    return obj.value;
   };
 
   list.contains = function(target){
+    var current = list.head;
+    while(current.next !== null) {
+      if(current.value === target) {
+        return true;
+      }
+      current = current.next;
+    }
+    return (current.value === target) ? true : false;
   };
-
   return list;
 };
 
@@ -27,3 +49,11 @@ var Node = function(value){
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+ // linkedList.addToTail(4);
+ // linkedList.addToTail(5);
+ // expect(linkedList.head.value).to.equal(4);
+ // linkedList.removeHead();
+ // expect(linkedList.head.value).to.equal(5);
+
