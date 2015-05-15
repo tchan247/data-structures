8558b63bbf49559710ee6e78d87bb6684005a1e9
tchan247 <tchan247@gmail.com>
2015-05-15 16:04:43 -0700

    Refactored LinkedList and Completed Tree

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,26 +2,22 @@ var LinkedList = function(){
   var list = {};
   list.head = null;
   list.tail = null;
-  var first = 0;
-  var last = 0;
 
   list.addToTail = function(value){
     var obj = new Node(value);
-    if(last > 0) {
-      list[last-1].next = obj;
+    if(!list.head) {
+      list.head = obj;
+    } else {
+      list.tail.next = obj;
     }
-    list[last] = obj;
+
     list.tail = obj;
-    last++;
-    list.head = list[first];
   };
 
   list.removeHead = function(){
-    var obj = list[first];
-    delete list[first];
-    first++;
-    list.head = list[first];
-    return obj.value;
+    var original = list.head;
+    list.head = original.next;
+    return original.value;
   };
 
   list.contains = function(target){
@@ -34,6 +30,7 @@ var LinkedList = function(){
     }
     return (current.value === target) ? true : false;
   };
+
   return list;
 };
 
@@ -46,14 +43,68 @@ var Node = function(value){
   return node;
 };
 
+
+// var LinkedList = function(){
+//   var list = {};
+//   list.head = null;
+//   list.tail = null;
+//   var first = 0;
+//   var last = 0;
+
+//   list.addToTail = function(value){
+//     var obj = new Node(value);
+//     if(last > 0) {
+//       list[last-1].next = obj;
+//     }
+//     list[last] = obj;
+//     list.tail = obj;
+//     last++;
+//     list.head = list[first];
+//   };
+
+//   list.removeHead = function(){
+//     var obj = list[first];
+//     delete list[first];
+//     first++;
+//     list.head = list[first];
+//     return obj.value;
+//   };
+
+//   list.contains = function(target){
+//     var current = list.head;
+//     while(current.next !== null) {
+//       if(current.value === target) {
+//         return true;
+//       }
+//       current = current.next;
+//     }
+//     return (current.value === target) ? true : false;
+//   };
+
+//   return list;
+// };
+
+// var Node = function(value){
+//   var node = {};
+
+//   node.value = value;
+//   node.next = null;
+
+//   return node;
+// };
+
 /*
  * Complexity: What is the time complexity of the above functions?
+ * addToTail: Constant Time O(1)
+ * removeHead: Constant Time O(1)
+ * contains: Linear Time O(n)
  */
 
+// Removing Node from Reverse Linked List
+ // find the element I want to delete
+ // store element.next in a var
+ // element.previous.next = var
+ // //
 
- // linkedList.addToTail(4);
- // linkedList.addToTail(5);
- // expect(linkedList.head.value).to.equal(4);
- // linkedList.removeHead();
- // expect(linkedList.head.value).to.equal(5);
+
 
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,28 +1,54 @@
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
-
+  for(var key in treeMethods){
+    newTree[key] = treeMethods[key];
+  }
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
 
-
-
-
-
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  var tree = Tree(value);
+  this.children.push(tree);
+  return tree;
 };
 
 treeMethods.contains = function(target){
+  var contains = false;
 
+  var inner = function(tree){
+    if(tree.value === target){
+      contains = true;
+    }
+
+    var children = tree.children;
+
+    if(!children){
+      return ;
+    }
+
+    for(var i=0; i<children.length; i++) {
+      inner(children[i]);
+    }
+  }
+  // debugger;
+  inner(this);
+
+  return contains;
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+// A tree class, in functional with shared methods style, with the following properties:
+// .children property, an array containing a number of subtrees
+// .addChild() method, takes any value, sets that as the target of a node, and adds that node as a child of the tree
+// A .contains() method, takes any input and returns a boolean reflecting whether it can be found as the value of the target node or any descendant node
+// What is the time complexity of the above functions?
