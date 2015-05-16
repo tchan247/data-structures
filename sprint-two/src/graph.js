5420e971e0a3c816e5116b72e27639f9b42c4f4c
tchan247 <tchan247@gmail.com>
2015-05-16 10:56:08 -0700

    Completed Graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -13,65 +13,80 @@ Graph.prototype.addNode = function(value){
 Graph.prototype.contains = function(value){
   var bool = false;
 
-  for(var i = 0; i < this.graph.length; i++) {
-    if(this.graph[i].value === value) {
+  this.forEachNode(function(val, node){
+    if(val === value) {
       bool = true;
     }
-  }
+  });
 
   return bool;
 };
 
 Graph.prototype.removeNode = function(value){
-  for(var i = 0; i < this.graph.length; i++) {
-    if(this.graph[i].value === value) {
-      this.graph.splice(i, 1);
+  this.forEachNode(function(val, node, index, graph){
+    if(val === value) {
+      graph.splice(index, 1);
     }
-  }
+  });
+
 };
 
 Graph.prototype.hasEdge = function(fromValue, toValue){
-  for(var i = 0; i < this.graph.length; i++) {
-    if((this.graph[i].value === fromValue) && (this.graph[i].edges.indexOf(toValue) > -1)) {
-      return true
+  var bool = false;
+
+  this.forEachNode(function(val, node, index){
+    if(val === fromValue && node.edges.indexOf(toValue) > -1){
+      bool = true;
     }
-  }
-  return false;
+  });
+
+  return bool;
+
 };
 
 Graph.prototype.addEdge = function(fromValue, toValue){
-  for(var i = 0; i < this.graph.length; i++) {
-    if(this.graph[i].value === fromValue) {
-      this.graph[i].edges.push(toValue);
+
+  this.forEachNode(function(val, node){
+    if(val === fromValue) {
+      node.edges.push(toValue);
     }
-    if(this.graph[i].value === toValue) {
-      this.graph[i].edges.push(fromValue);
+    if(val === toValue) {
+      node.edges.push(fromValue);
     }
-  }
+  });
+
 };
 
 Graph.prototype.removeEdge = function(fromValue, toValue){
 
   // FIGURE THIS OUT:
+  this.forEachNode(function(val, node){
+    if(val === fromValue){
+      node.edges.splice(node.edges.indexOf(toValue), 1);
+    }
+    if(val === toValue){
+      node.edges.splice(node.edges.indexOf(fromValue), 1);
+    }
 
-  for(var i = 0; i < this.graph.length; i++) {
-    if(this.graph[i].value === fromValue) {
-      this.graph[i].edges.splice(this.graph[this.graph.indexOf(toValue)], 1);
-    }
-    if(this.graph[i].value === toValue) {
-      this.graph[i].edges.push(fromValue);
-    }
-  }
+  });
+
 };
 
 Graph.prototype.forEachNode = function(cb){
   for(var i = 0; i < this.graph.length; i++) {
-    cb(this.graph[i]);
+    cb(this.graph[i].value, this.graph[i], i, this.graph);
   }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ *  addNode: O(1)
+ *  contains: O(n)
+ *  removeNode: O(n)
+ *  hasEdge: O(n)
+ *  addEdge: O(n)
+ *  removeEdge: O(n)
+ *  forEachNode: O(n)
  */
 
 
